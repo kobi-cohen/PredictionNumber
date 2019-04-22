6908608f65783227f3c3c0d39cb75d607bd7d467
Kobi Cohen <kobicohencom@gmail.com>
2019-04-22 03:48:58 +0300

    third change after responsive mobile phone modeles

--- script.js
+++ script.js
@@ -1,52 +1,25 @@
-// const value="19071";
-// const n=document.querySelectorAll('.number');
-// const o=document.querySelector('.output');
-// // const d=document.querySelector('.del'); // delet button
-// let i=0;
+
+let value="123456";
+const o=document.querySelector('.output'); // output section
+const n=document.querySelector('.numbers'); //numbers section
+
+const popup=document.querySelector('.popup');  // change popup screen
+const save=document.querySelector('#btn');  // save button
+const input=document.querySelector('#input'); // input field enter value
 
 
-// let onClick=()=>{
-//     if (value[i]!=undefined){
-//        o.textContent+=value[i];
-//        i=i+1;
-//     }   
-// };
-
-// // let del=()=>{
-// //     o.textContent=o.textContent.slice(0,length-1);
-// // }  // /delete function functionality
-// n.forEach(number=>{
-//     number.addEventListener('click' , onClick);
-// });
-
-// d.addEventListener('click',del);
-
-
-
-let value="12345";
-const o=document.querySelector('.output');
-const n=document.querySelector('.numbers');
-
-const popup=document.querySelector('.popup');
-const save=document.querySelector('#btn');
-const input=document.querySelector('#input');
-
-
-
-
-
-let i=0;
-let counter=0;
+let i=0; //the first char value.
+let counter=0; // indicator variable for click to change the value. 5 click needed.
 
 save.addEventListener('click',()=>{
     value=input.value;
     o.textContent='';
-    i=0;
-    popup.style.display="none";
+    i=0; // location of the next char in the value.
+    popup.style.display="none"; // hide the change screen
 })
 let onClick=()=>{
     if (value[i]!=undefined){
-       o.textContent+=value[i];
+       o.textContent+=value[i]; //add the next char
        i=i+1;
     }   
 };
@@ -54,11 +27,11 @@ let onClick=()=>{
 let changeValue=()=>{
     counter++;
     if (counter >=5) {
-            popup.style.display="block";
+            popup.style.display="block"; //show the popup screen
             counter=0;
     } 
 }
 
-o.addEventListener('click',changeValue);
+o.addEventListener('click',changeValue); // change event click
 
-  n.addEventListener('click' , onClick);
+n.addEventListener('click' , onClick); //display the next char screen
